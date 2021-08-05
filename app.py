5f8179d29146b35dba6cb0b6875829a8c2b619f6
Albert Navarro Gallinad <albert.navarro@adaptcentre.ie>
2021-08-05 14:59:34 +0100

    added extra round

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ ackCard = dbc.Card([
             Horizon 2020 research and innovation programme under the Marie Sklodowska-Curie Grant Agreement 
             No. 813545 at the [ADAPT Centre for Digital Content Technology](https://www.adaptcentre.ie/) (grant number 13/RC/2106 P2) at 
             Trinity College Dublin, collaborating with the [European Institute for Innovation through Health Data (i~HD)](https://www.i-hd.eu/). | 
-            Contact: albert.navarro@adaptcentre.ie'
+            Contact: albert.navarro@adaptcentre.ie
             ''', style={'font-size': '14px'},
                          className='card-text',
                          ),
@@ -144,6 +144,12 @@ nodesIDsMatrix = {'Blue': ['a12', 'a13', 'a14', 'a15'],
                   'Yellow': ['a22', 'a23', 'a24', 'a25'],
                   'Green': ['a32', 'a33', 'a34', 'a35'],
                   'Pink': ['a42', 'a43', 'a44', 'a45']}
+
+nodesIDsMatrix2 = {'Blue': {'male': ['a12', 'a13', 'a14', 'a15']},
+                   'Yellow': {'male': ['a22', 'a23', 'a24', 'a25']},
+                   'Green': {'female': ['a32', 'a33', 'a34', 'a35']},
+                   'Pink': {'female': ['a42', 'a43', 'a44', 'a45']}}
+
 '''
 # Randomly select country and patient for Grey view
 rndCountry_Grey = random.choice(countryColour)
@@ -191,7 +197,7 @@ frCard_Grid = dbc.Card(
             ),
         ], style={
             'background-image': 'url(https://raw.githubusercontent.com/navarral/QuessWho-Patient/master/GuessWho_Grid_Grey.jpg)',
-            'background-size': '37em 34em', #'600px 550px'
+            'background-size': '37em 34em',  # '600px 550px'
             'background-repeat': 'no-repeat',
             # 'marginBottom': '-5em'
         }, ),
@@ -254,6 +260,82 @@ srCard_Grid = dbc.Card(
         [
             html.Div([
                 cyto.Cytoscape(
+                    id='cyto_grey2',
+                    elements=nodes,
+                    autoungrabify=False,
+                    autolock=True,
+                    zoom=1,
+                    minZoom=1,
+                    maxZoom=1,
+                    panningEnabled=False,
+                    stylesheet=node_stylesheet,
+                ),
+            ], style={
+                'background-image': 'url(https://raw.githubusercontent.com/navarral/QuessWho-Patient/master/GuessWho_Grid_Grey.jpg)',
+                'background-size': '37em 34em',  # '600px 550px'
+                'background-repeat': 'no-repeat',
+                # 'marginBottom': '-5em'
+            }, ),
+        ]
+    ),
+    className='mt-3',
+)
+
+srCard_Doctor = dbc.Card(
+    dbc.CardBody(
+        [
+            html.H4('Can you help Dr. Data find the patient?', className='card-title'),
+            dcc.Markdown(id='hintGrey2',
+                         className="card-text",
+                         ),
+            html.Div([
+                dbc.Alert([
+                    html.H4("Oh that's not the patient!", className='alert-heading'),
+                    html.P('Please try again.'),
+                ],
+
+                    id='wrongChoiceGrey2',
+                    dismissable=True,
+                    is_open=False,
+                    color='danger',
+                    duration=2000,
+                ),
+                dbc.Alert(
+                    [
+                        html.H4('Well done!', className='alert-heading'),
+                        html.P("Yes, that's the patient! Thanks for your help."),
+                    ],
+                    id='rightChoiceGrey2',
+                    dismissable=False,
+                    is_open=False,
+                    color='success'
+                ), ]),
+            dbc.CardImg(id='grey2_doctor', className='image',
+                        src='data:image/png;base64, ' + pil_to_b64(pil_img_doctor),
+                        style={'height': '50%', 'width': '60%'},
+                        bottom=True),
+            dbc.Button('Next Round', color='primary', disabled=True,
+                       id='grey2Button'),
+        ]
+    ),
+
+    className='mt-3',
+)
+
+secondRoundTab = dbc.Row(
+    [
+        dbc.Col(width=1),
+        dbc.Col(srCard_Grid, width=7),
+        dbc.Col(srCard_Doctor, width=3),
+        dbc.Col(width=1),
+    ]
+)
+
+trCard_Grid = dbc.Card(
+    dbc.CardBody(
+        [
+            html.Div([
+                cyto.Cytoscape(
                     id='cyto_colour',
                     elements=nodes,
                     autoungrabify=False,
@@ -266,7 +348,7 @@ srCard_Grid = dbc.Card(
                 ),
             ], style={
                 'background-image': 'url(https://raw.githubusercontent.com/navarral/QuessWho-Patient/master/GuessWho_Grid.jpg)',
-                'background-size': '37em 34em', #'600px 550px'
+                'background-size': '37em 34em',  # '600px 550px'
                 'background-repeat': 'no-repeat',
                 # 'marginBottom': '-5em'
             }, ),
@@ -275,7 +357,7 @@ srCard_Grid = dbc.Card(
     className='mt-3',
 )
 
-srCard_Doctor = dbc.Card(
+trCard_Doctor = dbc.Card(
     dbc.CardBody(
         [
             html.H4('Can you help Dr. Data find the patient?', className='card-title'),
@@ -316,11 +398,11 @@ srCard_Doctor = dbc.Card(
     className='mt-3',
 )
 
-secondRoundTab = dbc.Row(
+thirdRoundTab = dbc.Row(
     [
         dbc.Col(width=1),
-        dbc.Col(srCard_Grid, width=7),
-        dbc.Col(srCard_Doctor, width=3),
+        dbc.Col(trCard_Grid, width=7),
+        dbc.Col(trCard_Doctor, width=3),
         dbc.Col(width=1),
     ]
 )
@@ -334,8 +416,8 @@ concCard = dbc.CardGroup(
                     dcc.Markdown('''
                     Today we learnt that people diagnosed with a rare disease are at an increased
                     risk of identification from research data because linking of a couple of factors may expose
-                    the patient's identity. However, if the adequate data protection regulations are applied
-                    researchers can conduct their research safely.         
+                    the patient's identity (Round 3). However, if the adequate data protection regulations are applied
+                    researchers can conduct their research safely (Rounds 1 and 2).         
                     ''',
                                  className="card-text",
                                  ),
@@ -379,6 +461,7 @@ tabs = dbc.Tabs(
         dbc.Tab(introTab, label='Introduction'),
         dbc.Tab(firstRoundTab, label='Round 1', disabled=True),
         dbc.Tab(secondRoundTab, label='Round 2', disabled=True),
+        dbc.Tab(thirdRoundTab, label='Round 3', disabled=True),
         dbc.Tab(conclusionTab, label='Conclusion', disabled=True),
     ], id='tabsID', active_tab='tab-0',
 )
@@ -395,6 +478,7 @@ app.layout = html.Div([tabs])
 
 @app.callback([Output('rndPatients', 'data'),
                Output('hintGrey', 'children'),
+               Output('hintGrey2', 'children'),
                Output('hintColour', 'children')],
               [Input('startButton', 'n_clicks')],
               )
@@ -406,16 +490,24 @@ def chooseRndCountryPatient(n_clicks_start):
         rndCountry_Grey = random.choice(countryColour)
         rightPatient_Grey = random.choice(nodesIDsMatrix[rndCountry_Grey])
 
+        # Randomly select country and patient for Grey 2 view
+        rndCountry_Grey2 = random.choice(countryColour)
+        rightPatient_Grey2 = random.choice(nodesIDsMatrix2[rndCountry_Grey2][str(list(nodesIDsMatrix2[rndCountry_Grey2].keys())[0])])
+
         # Randomly select country and patient for Colour view
         rndCountry_Colour = random.choice(countryColour)
         rightPatient_Colour = random.choice(nodesIDsMatrix[rndCountry_Colour])
 
         print('grey:', rightPatient_Grey)
+        print('grey2:', rightPatient_Grey2)
         print('colour:', rightPatient_Colour)
         # Store country and patient choice in a dict that will refresh as the page is refreshed
         rndDict = {
             'greyC': rndCountry_Grey,
             'greyP': rightPatient_Grey,
+            'grey2C': rndCountry_Grey2,
+            'grey2S': str(list(nodesIDsMatrix2[rndCountry_Grey2].keys())[0]),
+            'grey2P': rightPatient_Grey2,
             'colourC': rndCountry_Colour,
             'colourP': rightPatient_Colour}
 
@@ -424,28 +516,37 @@ def chooseRndCountryPatient(n_clicks_start):
 
         Please click on the faces on the left to help me find the patient!'''
 
+        hintGrey2 = '''
+        Hi! All I know is that the patient is a **''' + rndDict['grey2S'] + '''** **adult** that lives in the **''' + \
+                    rndDict['grey2C'] + ''' country** ...
+
+        Please click on the faces on the left to help me find the patient!'''
+
         hintColour = '''
         Hi! All I know is that the patient is an **adult** that lives in the **''' + rndDict['colourC'] + ''' country** ...
 
         Please click on the faces on the left to help me find the patient!'''
 
-        return [rndDict, hintGrey, hintColour]
+        return [rndDict, hintGrey, hintGrey2, hintColour]
 
 
 @app.callback(Output('tabsID', 'active_tab'),
               [Input('startButton', 'n_clicks'),
                Input('nextRound', 'n_clicks'),
+               Input('grey2Button', 'n_clicks'),
                Input('concButton', 'n_clicks')],
               )
-def goToRound1Tab(n_clicks_start, n_clicks_round1, n_clicks_round2):
+def goToRound1Tab(n_clicks_start, n_clicks_round1, n_clicks_round2, n_clicks_round3):
     if not n_clicks_start:
         raise PreventUpdate
-    if n_clicks_start and not n_clicks_round1 and not n_clicks_round2:
+    if n_clicks_start and not n_clicks_round1 and not n_clicks_round2 and not n_clicks_round3:
         return 'tab-1'
-    if n_clicks_start and n_clicks_round1 and not n_clicks_round2:
+    if n_clicks_start and n_clicks_round1 and not n_clicks_round2 and not n_clicks_round3:
         return 'tab-2'
-    if n_clicks_start and n_clicks_round1 and n_clicks_round2:
+    if n_clicks_start and n_clicks_round1 and n_clicks_round2 and not n_clicks_round3:
         return 'tab-3'
+    if n_clicks_start and n_clicks_round1 and n_clicks_round2 and n_clicks_round3:
+        return 'tab-4'
 
 
 @app.callback([Output('wrongChoiceGrey', 'is_open'),
@@ -471,6 +572,32 @@ def nextRoundButtonClick(rightAnswer_Ok):
         return [True]
     else:
         return [False]
+
+
+@app.callback([Output('wrongChoiceGrey2', 'is_open'),
+               Output('rightChoiceGrey2', 'is_open')],
+              [Input('cyto_grey2', 'tapNodeData'),
+               Input('rndPatients', 'data')])
+def guessGreyPatient(nodeID_label, rndChoice):
+    if not rndChoice:
+        raise PreventUpdate
+    else:
+        if not nodeID_label:
+            raise PreventUpdate
+        if nodeID_label['id'] != rndChoice['grey2P']:
+            return [True, False]
+        if nodeID_label['id'] == rndChoice['grey2P']:
+            return [False, True]
+
+
+@app.callback([Output('grey2Button', 'disabled')],
+              [Input('rightChoiceGrey', 'is_open')])
+def nextRoundButtonClick(rightAnswer_Ok):
+    if not rightAnswer_Ok:
+        return [True]
+    else:
+        return [False]
+
 
 
 @app.callback([Output('wrongChoiceColour', 'is_open'),
